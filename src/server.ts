e295c6c06fcd5b1ff96779fbb37caa5d4f97b70f
Janderson P. Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2021-08-03 16:33:35 -0300

    feat: Degubando a aplicação

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello Node.js' });
+});
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
 });
 
 app.listen(3333, () => {
