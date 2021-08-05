aaac51bbf5a3c0b0685ee4a01f2867816d24f938
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2021-08-05 20:54:52 -0300

    feat: Criando categoria

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,14 @@
 import express from 'express';
 
+/** Importing routes */
+import { categoriesRoutes } from './routes/categories.routes';
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Node.js' });
-});
-
-/** */
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
+/** Routes */
+app.use(categoriesRoutes);
 
 app.listen(3333, () => {
   console.log('✔ Server started on port 3333 ');
