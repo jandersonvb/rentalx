c5c3a19f0d293c778ad89a0b15beea0f451a0ba2
jandersonvb <jandersonvb.dev@gmail.com>
2022-12-06 21:40:02 -0300

    feat: reading upload data

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,10 +1,17 @@
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
 import { ImportCategoryController } from './ImportCategoryController';
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
-const importCategoryUseCase = new ImportCategoryUseCase();
+const categoriesRepository = CategoriesRepository.getInstance();
+
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 
 const importCategoryController = new ImportCategoryController(
   importCategoryUseCase,
 );
 
-export { importCategoryUseCase, importCategoryController };
+export {
+  categoriesRepository,
+  importCategoryUseCase,
+  importCategoryController,
+};
