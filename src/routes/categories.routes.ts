5e46e0b800f31907ab3ffb12c3dd7f1bb8b3bb7f
jandersonvb <jandersonvb.dev@gmail.com>
2022-12-08 16:34:36 -0300

    refactor: using docker compose

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,6 +12,7 @@ const upload = multer({
 });
 
 categoriesRoutes.post('/', (request, response) => {
+  console.log('REload Funcionando');
   return createCategoryController.handle(request, response);
 });
 
